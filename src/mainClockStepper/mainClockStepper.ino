92c6f24039fd10e4beba0d49d0a0d61707bc470a
Michael Shiloh <michael@michaelshiloh.com>
2023-05-10 17:58:45 +0400

    main clock controller

---
+++ src/mainClockStepper/mainClockStepper.ino
@@ -0,0 +1,14 @@
+
+const int WHICHHANDPIN 12
+const int WHICHDIRECTION PIN 13
+const int STEPPIN A0
+
+const int HOURIN0PIN 2
+const int HOURIN1PIN 3
+const int HOURIN2PIN 4
+const int HOURIN3PIN 5
+
+const int MININ0PIN 6
+const int MININ1PIN 7
+const int MININ2PIN 8
+const int MININ3PIN 9
