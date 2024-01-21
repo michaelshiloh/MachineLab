3925a992aa1bf64e6cf13f6d1a70f2c1c0bb48e0
Michael Shiloh <michael@michaelshiloh.com>
2024-01-21 17:12:00 +0400

    setting up for new semester

--- homework.md
+++ homework.md
@@ -11,7 +11,8 @@ Shortcut to [today's assignment](weeklySchedule.md#todays-assignment)
 
 Course website: [https://github.com/michaelshiloh/MachineLab](https://github.com/michaelshiloh/MachineLab)      
 Instructor: Michael Shiloh mshiloh@nyu.edu    
-Office hours: By appointment  
+Office hours: If you want to meet schedule an appointment [here](https://calendly.com/michaelshiloh/office_hours)
+
 Meeting times:    
 	2:10 PM - 3:25 PM Monday  
 	2:10 PM - 4:50 PM Wednesday  
@@ -22,3 +23,7 @@ Class location: C3-029 (IM Lab)
 
 **Everything here is subject to change**
 
+
+This document: Homework
+
+## Homework due Wednesday 24 January 2023
--- src/mainClockMega/mainClockMega.ino
+++ src/mainClockMega/mainClockMega.ino
@@ -2,72 +2,140 @@
 
 RTC_DS1307 rtc;
 
-const int BOATSTRIGGERPIN 2
-const int MYTHSTRIGGERPIN 3
-const int FALCONSTRIGGERPIN 4
-const int SANDTRIGGERPIN 5
-const int BLOCKSTRIGGERPIN 6
+// Trigger signals to the modules
+const int BOATSTRIGGERPIN = 2;
+const int MYTHSTRIGGERPIN = 3;
+const int FALCONSTRIGGERPIN = 4;
+const int SANDTRIGGERPIN = 5;
+const int BLOCKSTRIGGERPIN = 6;
+const int CUBETRIGGERPIN = 12;
 
-const int BOATSDONEPIN 7
-const int MYTHSDONEPIN 8
-const int FALCONSDONEPIN 9
-const int SANDDONEPIN 10
-const int BLOCKSDONEPIN 11
+// Done signals from the modules
+const int BOATSDONEPIN = 7;
+const int MYTHSDONEPIN = 8;
+const int FALCONSDONEPIN = 9;
+const int SANDDONEPIN = 10;
+const int BLOCKSDONEPIN = 11;
 
-const int HOURIN0PIN 12
-const int HOURIN1PIN 13
-const int HOURIN2PIN 14
-const int HOURIN3PIN 15
 
-const int MININ0PIN 16
-const int MININ1PIN 17
-const int MININ2PIN 18
-const int MININ3PIN 19
+const int HOURIN1PIN = 38;
+const int HOURIN2PIN = 40;
+const int HOURIN3PIN = 42;
+const int HOURIN4PIN = 44;
 
-const int MINHANDZEROPIN 22
-const int HOURHANDZEROPIN 23
+const int MININ1PIN = 50;
+const int MININ2PIN = 48;
+const int MININ3PIN = 52;
+const int MININ4PIN = 46;
+
+// RTC connections
+const int RTCPOWERa = 18;
+const int RTCPOWERb = 19;
+// Pins 20 and 21 are SPI to the RTC
+
+const int MINHANDZEROPIN = 30;
+const int HOURHANDZEROPIN = 32;
 
 //Use these Global Variables in your code
 int currentTimeHours;
 int currentTimeMinutes;
 int currentTimeSeconds;
 
-void setup () {
- 
+void setup() {
+
   Serial.begin(9600);
 
 #ifndef ESP8266
-  while (!Serial); // wait for serial port to connect. 
+  while (!Serial)
+    ;  // wait for serial port to connect.
 #endif
 
-  if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    Serial.flush();
-    abort();
-  }
+  // if (!rtc.begin()) {
+  //   Serial.println("Couldn't find RTC");
+  //   Serial.flush();
+  //   abort();
+  // }
 
+  pinMode(MININ1PIN, OUTPUT);
+  pinMode(MININ2PIN, OUTPUT);
+  pinMode(MININ3PIN, OUTPUT);
+  pinMode(MININ4PIN, OUTPUT);
 
+  pinMode(HOURIN1PIN, OUTPUT);
+  pinMode(HOURIN2PIN, OUTPUT);
+  pinMode(HOURIN3PIN, OUTPUT);
+  pinMode(HOURIN4PIN, OUTPUT);
+}
+
+void loop() {
+  //Re-assigning variables every second
+  // DateTime now = rtc.now();
+  // currentTimeHours = now.hour();
+  // currentTimeMinutes = now.minute();
+  // currentTimeSeconds = now.second();
+
+  //For testing purposes
+  Serial.print(currentTimeHours);
+  Serial.print(":");
+  Serial.print(currentTimeMinutes);
+  Serial.print(":");
+  Serial.print(currentTimeSeconds);
+  Serial.println();
+
+   oneCycleMin(2);
+   oneCycleHour(2);
  
 }
 
-void loop () {
-    //Re-assigning variables every second
-    DateTime now = rtc.now();
-    currentTimeHours = now.hour(); 
-    currentTimeMinutes = now.minute();
-    currentTimeSeconds = now.second(); 
-  
-//For testing purposes
-    Serial.print(currentTimeHours);
-    Serial.print(":");
-    Serial.print(currentTimeMinutes);
-    Serial.print(":");
-    Serial.print(currentTimeSeconds);
-    Serial.println(); 
+void oneCycleMin(int delayAmount) {
+  digitalWrite(MININ1PIN, LOW);
+  digitalWrite(MININ2PIN, HIGH);
+  digitalWrite(MININ3PIN, LOW);
+  digitalWrite(MININ4PIN, HIGH);
+  delay(delayAmount);
 
-    delay(1000);
+  digitalWrite(MININ1PIN, HIGH);
+  digitalWrite(MININ2PIN, LOW);
+  digitalWrite(MININ3PIN, LOW);
+  digitalWrite(MININ4PIN, HIGH);
+  delay(delayAmount);
 
-    
-      
-    
+  digitalWrite(MININ1PIN, HIGH);
+  digitalWrite(MININ2PIN, LOW);
+  digitalWrite(MININ3PIN, HIGH);
+  digitalWrite(MININ4PIN, LOW);
+  delay(delayAmount);
+
+  digitalWrite(MININ1PIN, LOW);
+  digitalWrite(MININ2PIN, HIGH);
+  digitalWrite(MININ3PIN, HIGH);
+  digitalWrite(MININ4PIN, LOW);
+  delay(delayAmount);
+}
+
+
+void oneCycleHour(int delayAmount) {
+  digitalWrite(HOURIN1PIN, LOW);
+  digitalWrite(HOURIN2PIN, HIGH);
+  digitalWrite(HOURIN3PIN, LOW);
+  digitalWrite(HOURIN4PIN, HIGH);
+  delay(delayAmount);
+
+  digitalWrite(HOURIN1PIN, HIGH);
+  digitalWrite(HOURIN2PIN, LOW);
+  digitalWrite(HOURIN3PIN, LOW);
+  digitalWrite(HOURIN4PIN, HIGH);
+  delay(delayAmount);
+
+  digitalWrite(HOURIN1PIN, HIGH);
+  digitalWrite(HOURIN2PIN, LOW);
+  digitalWrite(HOURIN3PIN, HIGH);
+  digitalWrite(HOURIN4PIN, LOW);
+  delay(delayAmount);
+
+  digitalWrite(HOURIN1PIN, LOW);
+  digitalWrite(HOURIN2PIN, HIGH);
+  digitalWrite(HOURIN3PIN, HIGH);
+  digitalWrite(HOURIN4PIN, LOW);
+  delay(delayAmount);
 }
